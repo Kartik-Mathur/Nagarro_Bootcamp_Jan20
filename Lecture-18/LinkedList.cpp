dd626aa135bc352817dd71094bfd7aa3967d9c19
Kartik Mathur <kartik.mathur@codingblocks.com>
2020-03-23 20:50:44 +0530

    Lecture-18

--- Lecture-18/LinkedList.cpp
+++ Lecture-18/LinkedList.cpp
@@ -13,6 +13,15 @@ public:
 	}
 };
 
+int lengthLL(node* head){
+	int count = 0;
+	while(head){
+		count++;
+		head = head->next;
+	}
+	return count;
+}
+
 void InsertAtFront(node* &head,node* &tail,int data){
 	if(head == NULL){
 		head = tail = new node(data);
@@ -24,8 +33,40 @@ void InsertAtFront(node* &head,node* &tail,int data){
 	}
 }
 
+void InsertAtEnd(node* &head,node* &tail,int data){
+	if(head == NULL){
+		head = tail = new node(data);
+	}
+	else{
+		node*n = new node(data);
+		tail->next = n;
+		tail = n;
+	}
+}
+
+void InsertAtMid(node* &head,node* &tail,int pos,int data){
+	if(pos == 0){
+		InsertAtFront(head,tail,data);
+	}
+	else if(pos>=lengthLL(head)-1){
+		InsertAtEnd(head,tail,data);
+	}
+	else{
+		node* temp = head;
+		int count = 1;
+		while(count<=pos-1){
+			temp = temp->next;
+			count++;
+		}
+
+		node* n = new node(data);
+		n->next = temp->next;
+		temp->next = n;
+	}
+}
 
 void Print(node* head){
+
 	while(head){
 		cout<<head->data<<"-->";
 		head = head->next;
@@ -47,6 +88,12 @@ int main(){
 	InsertAtFront(head,tail,1);
 
 	Print(head);
+	InsertAtEnd(head,tail,6);
+	InsertAtEnd(head,tail,7);
+	InsertAtEnd(head,tail,8);
+
+	Print(head);
+	InsertAtMid(head,tail,100,100);
 	Print(head);
 
 
