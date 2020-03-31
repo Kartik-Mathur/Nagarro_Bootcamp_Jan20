113c780133cf1676f14b2a2ce5f9db9838575f0a
Kartik Mathur <kartik.mathur@codingblocks.com>
2020-03-31 21:03:36 +0530

    Lecture-21

--- Lecture-21/BinaryTrees.cpp
+++ Lecture-21/BinaryTrees.cpp
@@ -59,6 +59,62 @@ void PostOrder(node* root){
 	cout<<root->data<<' ';
 }
 
+int CountNodes(node* root){
+	if(root == NULL){
+		return 0;
+	}
+
+	return CountNodes(root->left)+CountNodes(root->right)+1;
+}
+
+int height(node* root){
+	if(root ==NULL){
+		return 0;
+	}
+
+	int left_height = height(root->left);
+	int right_height = height(root->right);
+	return max(left_height,right_height)+1;
+}
+
+int diameter(node* root){
+	if(root == NULL){
+		return 0;
+	}
+
+	int op1 = height(root->left)+height(root->right);
+	int op2 = diameter(root->left);
+	int op3 = diameter(root->right);
+
+	return max(op1,max(op2,op3));
+}
+
+class Pair{
+public:
+	int height;
+	int diameter;
+};
+
+Pair FastDiameter(node* root){
+	Pair p;
+	// base case
+	if(root == NULL){
+		p.height = p.diameter = 0;
+		return p;
+	}
+
+	// Recursive case
+	Pair left = FastDiameter(root->left);
+	Pair right = FastDiameter(root->right);
+
+	p.height = max(left.height,right.height)+1;
+	int op1 = left.height+right.height;
+	int op2 = left.diameter;
+	int op3 = right.diameter;
+	p.diameter = max(op1,max(op2,op3));
+	return p;
+}
+
 int main(){
 	node* root = NULL;
 
@@ -69,7 +125,13 @@ int main(){
 	cout<<endl;
 	PostOrder(root);
 	cout<<endl;
+	cout<<"Number of Nodes : "<<CountNodes(root)<<endl;
+	cout<<"Height : "<<height(root)<<endl;
+	cout<<"Diameter : "<<diameter(root)<<endl;
+	Pair ans = FastDiameter(root);
 
+	cout<<"Fast Height : "<<ans.height<<endl;
+	cout<<"Fast Diameter : "<<ans.diameter<<endl;
 
 
 	return 0;
